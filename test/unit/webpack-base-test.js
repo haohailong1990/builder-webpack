09ea288e7bea29a3aadc9bf145a105024f7b681a
haohailong <haohailong@aikucun.com>
2020-05-22 19:38:50 +0800

    sve

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 describe('webpack.base.js test case', () => {
     const baseConfig = require('../../lib/webpack.base.js')
     it('entry', () => {
-        assert.equal(baseConfig.entry.index, '/Users/aikucun/workspace/webpack/my-project/20构建共配置包设计/builder-webpack/test/smoke/template/src/index/index.js')
-        assert.equal(baseConfig.entry.search, '/Users/aikucun/workspace/webpack/my-project/20构建共配置包设计/builder-webpack/test/smoke/template/src/search/index.js')
+        assert.equal(baseConfig.entry.index, '/Users/aikucun/workspace/webpack/my-project/21持续集成和Travis Ci/builder-webpack/test/smoke/template/src/index/index.js')
+        assert.equal(baseConfig.entry.search, '/Users/aikucun/workspace/webpack/my-project/21持续集成和Travis Ci/builder-webpack/test/smoke/template/src/search/index.js')
     })
 })
