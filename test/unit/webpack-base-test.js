dbd96cbac45ddec419d2ebef12c7f241d6477f9d
haohailong <haohailong@aikucun.com>
2020-05-22 19:56:42 +0800

    save

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 describe('webpack.base.js test case', () => {
     const baseConfig = require('../../lib/webpack.base.js')
     it('entry', () => {
-        assert.equal(baseConfig.entry.index, '/Users/aikucun/workspace/webpack/my-project/21持续集成和Travis Ci/builder-webpack/test/smoke/template/src/index/index.js')
-        assert.equal(baseConfig.entry.search, '/Users/aikucun/workspace/webpack/my-project/21持续集成和Travis Ci/builder-webpack/test/smoke/template/src/search/index.js')
+        assert.equal(baseConfig.entry.index.indexOf('builder-webpack/test/smoke/template/src/index/index.js') > -1,  true)
+        assert.equal(baseConfig.entry.search.indexOf('builder-webpack/test/smoke/template/src/search/index.js') > -1, true)
     })
 })
